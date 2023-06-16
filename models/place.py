7a284a0222dff9da1b3603cb01013548c5cd9bb4
thedavidemmmanuel <d.emmanuel@alustudent.com>
2023-06-16 23:49:33 +0200

    add

--- models/place.py
+++ models/place.py
@@ -2,13 +2,18 @@
 """ Place Module for HBNB project """
 
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
 
 
 class Place(BaseModel, Base):
     """ Place class """
     __tablename__ = 'places'
+
+    place_amenity = Table('place_amenity', Base.metadata,
+                          Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
+                          Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False)
+                          )
 
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
@@ -20,4 +25,4 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float)
     longitude = Column(Float)
-    reviews = relationship("Review", backref="place", cascade="all, delete")
+    amenities = relationship("Amenity", secondary='place_amenity', viewonly=False)
