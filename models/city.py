14ed714aad6368deabb731eeb64bd1f7026accf7
thedavidemmmanuel <d.emmanuel@alustudent.com>
2023-06-16 23:42:48 +0200

    add

--- models/city.py
+++ models/city.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
+
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
+    """ City class """
     __tablename__ = 'cities'
 
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
-    places = relationship("Place", backref="cities", cascade="all, delete")
+    places = relationship("Place", backref="city", cascade="all, delete")
