a1dda7b4f668b3be07ce4a1e389afa52b8264166
thedavidemmmanuel <d.emmanuel@alustudent.com>
2023-06-16 23:45:34 +0200

    add

--- models/review.py
+++ models/review.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-""" Review module for the HBNB project """
+""" Review Module for HBNB project """
+
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, ForeignKey, String
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class Review(BaseModel, Base):
-    """ Review class to store review information """
+    """ Review class """
     __tablename__ = 'reviews'
 
-    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
-    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
     text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
