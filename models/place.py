cbf5927f7ecaf77d51ff6df03c31036c5b394295
thedavidemmmanuel <d.emmanuel@alustudent.com>
2023-06-16 23:46:55 +0200

    add

--- models/place.py
+++ models/place.py
@@ -20,3 +20,4 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float)
     longitude = Column(Float)
+    reviews = relationship("Review", backref="place", cascade="all, delete")
