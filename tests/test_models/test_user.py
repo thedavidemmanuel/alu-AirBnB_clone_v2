a218a4b332a231288ca408e185fbf84d5e100a4b
thedavidemmmanuel <d.emmanuel@alustudent.com>
2023-06-16 19:46:17 +0200

    Added all files

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" """
+from tests.test_models.test_base_model import TestBaseModel
+from models.user import User
+
+
+class test_User(TestBaseModel):
+    """ """
+
+    def __init__(self, *args, **kwargs):
+        """ """
+        super().__init__(*args, **kwargs)
+        self.name = "User"
+        self.value = User
+
+    def test_first_name(self):
+        """ """
+        new = self.value()
+        new.first_name = "Jeff" 
+        self.assertEqual(type(new.first_name), str)
+
+    def test_last_name(self):
+        """ """
+        new = self.value()
+        new.last_name = "jeff"
+        self.assertEqual(type(new.last_name), str)
+
+    def test_email(self):
+        """ """
+        new = self.value()
+        new.email = "hi@dauda.studio"
+        self.assertEqual(type(new.email), str)
+
+    def test_password(self):
+        """ """
+        new = self.value()
+        new.password = "testpassword"
+        self.assertEqual(type(new.password), str)
